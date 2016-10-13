58d82ccc9a8a2031d43a90e8aaaa9285e7f66b7f
AaronXu <woshixwm@gmail.com>
2016-10-13 22:55:41 +0800

    第一次提交

---
+++ app/src/main/java/com/aaronxu/friendapartment/MainActivity.java
@@ -0,0 +1,89 @@
+package com.aaronxu.friendapartment;
+
+import android.graphics.Color;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.jude.rollviewpager.RollPagerView;
+import com.jude.rollviewpager.adapter.StaticPagerAdapter;
+import com.jude.rollviewpager.hintview.ColorPointHintView;
+import com.jude.rollviewpager.hintview.IconHintView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private ViewPager mViewPager;
+    private List<Fragment> fragmentList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
+        initFragment();
+        initMainViewPager();
+
+    }
+
+    private void initFragment() {
+        fragmentList = new ArrayList<>();
+        Fragment fragment1 = MainFragment.newInstance("首页");
+        fragmentList.add(fragment1);
+        Log.d(TAG, "initFragment: ");
+    }
+
+    private void initMainViewPager() {
+        FragmentPagerAdapter mAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return fragmentList.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return fragmentList.size();
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return getItem(position).getArguments().getString("title");
+            }
+        };
+        mViewPager.setAdapter(mAdapter);
+    }
+
+
+    public void firstClick(View v){
+        Toast.makeText(this,"到南昌",Toast.LENGTH_SHORT).show();
+    }
+    public void mostDecorate(View v){
+        Toast.makeText(this,"最装修",Toast.LENGTH_SHORT).show();
+    }
+    public void mostHuamnqi(View v){
+        Toast.makeText(this,"最人气",Toast.LENGTH_SHORT).show();
+    }
+    public void mostBest(View v){
+        Toast.makeText(this,"最优质",Toast.LENGTH_SHORT).show();
+    }
+    public void mostNew(View v){
+        Toast.makeText(this,"最新品",Toast.LENGTH_SHORT).show();
+    }
+    public void mostHot(View v){
+        Toast.makeText(this,"最火爆",Toast.LENGTH_SHORT).show();
+    }
+    public void mostArgue(View v){
+        Toast.makeText(this,"最争议",Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/aaronxu/friendapartment/MainFragment.java
@@ -0,0 +1,99 @@
+package com.aaronxu.friendapartment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.jude.rollviewpager.RollPagerView;
+import com.jude.rollviewpager.adapter.StaticPagerAdapter;
+import com.jude.rollviewpager.hintview.IconHintView;
+
+/**
+ * Created by AaronXu on 2016-10-13.
+ */
+public class MainFragment extends Fragment {
+
+    private static final String TAG = "MainFragment";
+    private RollPagerView mRollViewPager;
+    private View rootView;
+    private Context mContext;
+
+    public MainFragment(){
+
+    }
+    public static MainFragment newInstance(String title){
+        MainFragment fragment = new MainFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("title",title);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: ");
+        mContext = getContext();
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_main,container,false);
+        initRollViewPager();
+
+        return rootView;
+    }
+
+    private void initRollViewPager() {
+        mRollViewPager = (RollPagerView) rootView.findViewById(R.id.roll_view_pager);
+        //设置播放时间间隔
+        mRollViewPager.setPlayDelay(3000);
+        //设置透明度
+        mRollViewPager.setAnimationDurtion(500);
+        //设置适配器
+        mRollViewPager.setAdapter(new TestNormalAdapter());
+        //设置指示器（顺序依次）
+        //自定义指示器图片
+        //设置圆点指示器颜色
+        //设置文字指示器
+        //隐藏指示器
+        mRollViewPager.setHintView(new IconHintView(getContext(), R.mipmap.activate, R.mipmap.unactivate));
+        //mRollViewPager.setHintView(new ColorPointHintView(this, Color.YELLOW, Color.WHITE));
+        //mRollViewPager.setHintView(new TextHintView(this));
+        //mRollViewPager.setHintView(null);
+    }
+
+    private class TestNormalAdapter extends StaticPagerAdapter {
+        private int[] imgs = {
+                R.drawable.drawable1,
+                R.drawable.drawable2,
+                R.drawable.drawable3,
+                R.drawable.drawable4,
+        };
+
+
+        @Override
+        public View getView(ViewGroup container, int position) {
+            ImageView view = new ImageView(container.getContext());
+            view.setImageResource(imgs[position]);
+            view.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            return view;
+        }
+
+
+        @Override
+        public int getCount() {
+            return imgs.length;
+        }
+    }
+
+}
